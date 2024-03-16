a28185b9834f388db674939da372838e5374ae75
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-15 19:07:03 -0600

    remove submodule

---
+++ git-tag.sh
@@ -0,0 +1,8 @@
+git tag cryptoh/v1.0.4
+git tag databaseh/v1.0.4
+git tag encodingh/v1.0.4
+git tag logh/v1.0.4
+git tag mathh/v1.0.4
+git tag neth/v1.0.4
+git tag osh/v1.0.4
+git push origin --tags
---
+++ tags.sh
@@ -0,0 +1,8 @@
+git tag cryptoh/v1.0.3
+git tag databaseh/v1.0.3
+git tag encodingh/v1.0.3
+git tag logh/v1.0.3
+git tag mathh/v1.0.3
+git tag neth/v1.0.3
+git tag osh/v1.0.3
+git push origin --tags
