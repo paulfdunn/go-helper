93779339b83e1f34bc7de5520ade7afda8a3c52c
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-15 19:48:57 -0600

    Fix go-update.sh and update packages

--- git-tag.sh
+++ git-tag.sh
@@ -1,8 +1,8 @@
-git tag cryptoh/v1.0.5
-git tag databaseh/v1.0.5
-git tag encodingh/v1.0.5
-git tag logh/v1.0.5
-git tag mathh/v1.0.5
-git tag neth/v1.0.5
-git tag osh/v1.0.5
+git tag cryptoh/v1.0.6
+git tag databaseh/v1.0.6
+git tag encodingh/v1.0.6
+git tag logh/v1.0.6
+git tag mathh/v1.0.6
+git tag neth/v1.0.6
+git tag osh/v1.0.6
 git push origin --tags
