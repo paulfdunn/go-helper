60308452976fa87fe776ef50d3a2be023e1dea0d
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-16 11:45:27 -0600

    Move slice functions to slicesh

--- osh/osh_test.go
+++ osh/osh_test.go
@@ -8,33 +8,6 @@ import (
 	"testing"
 )
 
-func ExampleByteSliceToIntSlice() {
-	fmt.Println(ByteSliceToIntSlice([]byte{}))
-	fmt.Println(ByteSliceToIntSlice([]byte{0}))
-	fmt.Println(ByteSliceToIntSlice([]byte{0, 1, 2, 3, 4, 5}))
-
-	// Output:
-	// []
-	// [0]
-	// [0 1 2 3 4 5]
-}
-
-func ExampleByteSliceToString() {
-	fmt.Print(ByteSliceToString([]byte{}, 3))
-	fmt.Print(ByteSliceToString([]byte{0}, 3))
-	fmt.Print(ByteSliceToString([]byte{0, 1, 2}, 3))
-	fmt.Print(ByteSliceToString([]byte{0, 1, 2, 3}, 3))
-	fmt.Print(ByteSliceToString([]byte{0, 1, 2, 3, 4, 5}, 3))
-
-	// Output:
-	// 00
-	// 00 01 02
-	// 00 01 02
-	// 03
-	// 00 01 02
-	// 03 04 05
-}
-
 func ExampleDirIsEmpty() {
 	u, _ := user.Current()
 	b, _ := DirIsEmpty(u.HomeDir)
