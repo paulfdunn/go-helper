fb837daaf61b08a620579b1ed328020bd4a0dde5
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-16 06:44:45 -0600

    Add RemoveAllFiles

--- osh/osh.go
+++ osh/osh.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 // ByteSliceToIntSlice converts an byte slice to integer slice
@@ -61,4 +62,18 @@ func DirIsEmpty(path string) (bool, error) {
 	}
 
 	return false, err
+}
+
+// RemoveAllFiles will remove all files meeting the Glob pattern.
+func RemoveAllFiles(pattern string) error {
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			return err
+		}
+	}
+	return nil
 }
