ead210436782a7394896282e29c504f07e4b03af
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-30 07:13:25 -0600

    {Remove dependent versioning}

--- git-commit-and-tag.sh
+++ git-commit-and-tag.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Update VERSION and VERSION_WITH_DEPENDENCIES, then run to update update the repo (local and remote)
-# with the tags. Accepts a quotes string for the commit message
-# Note that the versioning scheme is that with no interdependencies get the odd minor version, then
-# packages with interdependencies get the next higher even version.
-VERSION="v1.3.16"
-VERSION_WITH_DEPENDENCIES="v1.4.16"
+# Update VERSION then run to update update the repo (local and remote)
+# with the tags. Accepts a quotes string for the commit message.
+# For packages with dependencies, the tip of master and the tag will
+# be the same code, but the tip will have the updates for packages with
+# dependencies.
+VERSION="v1.5.0"
 
 git add -A
 git commit -m "{$1}"
@@ -39,19 +39,19 @@ cd ../
 git add -A
 git commit -m 'Update packages with dependencies'
 git push origin
-git tag archiveh/"${VERSION_WITH_DEPENDENCIES}"
-git tag databaseh/"${VERSION_WITH_DEPENDENCIES}"
-git tag neth/"${VERSION_WITH_DEPENDENCIES}"
+git tag archiveh/"${VERSION}"
+git tag databaseh/"${VERSION}"
+git tag neth/"${VERSION}"
 git push origin --tags
 
 echo "\n\nUse the below to update packages"
-echo "go get -u github.com/paulfdunn/go-helper/archiveh@${VERSION_WITH_DEPENDENCIES}; go mod tidy"
+echo "go get -u github.com/paulfdunn/go-helper/archiveh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/cryptoh@${VERSION}; go mod tidy"
-echo "go get -u github.com/paulfdunn/go-helper/databaseh@${VERSION_WITH_DEPENDENCIES}; go mod tidy"
+echo "go get -u github.com/paulfdunn/go-helper/databaseh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/encodingh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/logh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/mathh@${VERSION}; go mod tidy"
-echo "go get -u github.com/paulfdunn/go-helper/neth@${VERSION_WITH_DEPENDENCIES}; go mod tidy"
+echo "go get -u github.com/paulfdunn/go-helper/neth@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/osh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/slicesh@${VERSION}; go mod tidy"
 echo "go get -u github.com/paulfdunn/go-helper/testingh@${VERSION}; go mod tidy"
